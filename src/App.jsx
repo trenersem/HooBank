b3b23cc8b1ba9e35b7edbca238e69b1e5f296f2c
MacBook <air@MacBook-Air-MacBook.local>
2022-10-02 22:13:25 +0300

    v0.2 add prettier.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
+} from './components';
 import style from './style';
 
 const App = () => (
-	<div className='bg-primary w-full overflow-hidden'>
-		<div className={`${style.paddingX} ${style.flexCenter}`}>
-			<div className={`${style.boxWidth}`}>
-					<Navbar/>
-			</div>
-		</div>
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${style.paddingX} ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
 
-		<div className={`bg-primary ${style.flexStart}`}>
-			<div className={`${style.boxWidth}`}>
-					<Hero/>
-			</div>
-		</div>
+    <div className={`bg-primary ${style.flexStart}`}>
+      <div className={`${style.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
 
-		<div className={`bg-primary ${style.flexCenter} ${style.paddingX}`}>
-			<div className={`${style.boxWidth}`}>
-					<Stats/>
-					<Business/>
-					<Billing/>
-					<CardDeal/>
-					<Testimonials/>
-					<Clients/>
-					<CTA/>
-					<Footer/>
-			</div>
-		</div>
-
-	</div>
+    <div className={`bg-primary ${style.flexCenter} ${style.paddingX}`}>
+      <div className={`${style.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
 );
 
 export default App;
